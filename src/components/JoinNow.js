942ce636120b8517345f7f2f51c52740d50df69e
Hasnain <mrajhasnain101@gmail.com>
2024-01-18 01:02:19 +0600

    others plus picture name was changed

---
+++ src/components/JoinNow.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JoinNow = () => {
+  return (
+    <div class="border-4 border-b-gray-950 w-96">Hi join now</div>
+  )
+}
+
+export default JoinNow
---
+++ src/components/Party.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PartyImg from "../assets/party.png";
+const Party = () => {
+  return (
+    <div class="border-4 border-blue-500 py-7">
+      <div class="relative">
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-35 rounded-lg"></div>
+        <div className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 text-center text-white w-9/12 border-4">
+          <div>
+            <h2 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+              Happy New Year
+            </h2>
+            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-2">
+              New Year Celebration Party
+            </h1>
+          </div>
+        </div>
+        <img src={PartyImg} alt="party" />
+      </div>
+    </div>
+  );
+};
+
+export default Party;
