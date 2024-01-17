b3a6a39d5473ad95276da36aac42ad0ac3e10f31
Hasnain <mrajhasnain101@gmail.com>
2024-01-18 01:01:51 +0600

    Deal is done

--- src/components/Deal.js
+++ src/components/Deal.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import DealImg from "../assets/deal.png";
 
 const Deal = () => {
   return (
-    <div>Deal</div>
-  )
-}
+    <div class="border-4 border-blue-500 py-7">
+      <div class="border border-yellow-500 flex items-center flex-col w-9/12">
+        <div class="border border-red-300 w-3/4 h-34 flex flex-col items-center">
+          <h1 class="text-4xl font-bold text-red-600">65 % OFF</h1>
+          <div>
+            <p>our holiday deals are better</p>
+            <p class="text-base font-bold text-black">All categories</p>
+          </div>
+        </div>
+        <div>
+          <img src={DealImg} alt="deal" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Deal
+export default Deal;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Banner from './Banner'
+import Deal from './Deal'
+import Party from './Party'
+import JoinNow from './JoinNow'
 
 const Home = () => {
   return (
-    <div>
-        <Banner/>
+    <div class='border-4 border-solid border-red-500 flex justify-center' >
+      <div class='w-10/12 border-4 border-yellow-200 '>
+      <Banner/>
+      <Deal/>
+      <Party/>
+      <JoinNow/>
+      </div>
     </div>
   )
 }
