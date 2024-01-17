efcff2c9bbbd61424a8627c50711e2a5723985a6
Hasnain <mrajhasnain101@gmail.com>
2024-01-18 01:01:11 +0600

    Banner completed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,14 +3,18 @@ import BannerImg from "../assets/Banner.png";
 
 const Banner = () => {
   return (
-    <div className="border-4 border-red-200 flex items-center justify-center py-14">
-      <div className="border-4 w-4/5 relative overflow-hidden">
-        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-35"></div>
-        <div className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 text-center text-white">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-2">Happy New Year</h1>
-          <h2 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">New Year Celebration Party</h2>
+    <div className=" flex items-center justify-center py-7 border-4 ">
+      <div className="relative">
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-35 rounded-lg"></div>
+        <div className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 text-center text-white w-9/12" >
+          <h2 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+          Happy New Year
+          </h2>
+          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-2">
+            New Year Celebration Party
+          </h1>
         </div>
-        <img className="w-full h-auto " src={BannerImg} alt="banner" />
+        <img className="w-full" src={BannerImg} alt="banner" />
       </div>
     </div>
   );
