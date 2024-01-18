fc303233b58bc8717b8aaafa59d0b8b54e403732
Hasnain <mrajhasnain101@gmail.com>
2024-01-18 18:34:44 +0600

    deal

--- src/components/Deal.js
+++ src/components/Deal.js
@@ -3,9 +3,9 @@ import DealImg from "../assets/deal.png";
 
 const Deal = () => {
   return (
-    <div class="border-4 border-blue-500 py-7">
-      <div class="border border-yellow-500 flex items-center flex-col w-9/12">
-        <div class="border border-red-300 w-3/4 h-34 flex flex-col items-center">
+    <div class=" py-7">
+      <div class=" flex items-center flex-col w-7/12">
+        <div class=" w-3/4 h-34 flex flex-col items-center">
           <h1 class="text-4xl font-bold text-red-600">65 % OFF</h1>
           <div>
             <p>our holiday deals are better</p>
