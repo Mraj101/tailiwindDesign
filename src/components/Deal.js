59d0c0274c467aacba868325e30fdd9a1bb14571
Hasnain <mrajhasnain101@gmail.com>
2024-01-21 19:19:34 +0600

    deals

--- src/components/Deal.js
+++ src/components/Deal.js
@@ -3,16 +3,16 @@ import DealImg from "../assets/deal.png";
 
 const Deal = () => {
   return (
-    <div class=" py-7">
-      <div class=" flex items-center flex-col w-7/12">
-        <div class=" w-3/4 h-34 flex flex-col items-center">
-          <h1 class="text-4xl font-bold text-red-600">65 % OFF</h1>
-          <div>
-            <p>our holiday deals are better</p>
-            <p class="text-base font-bold text-black">All categories</p>
+    <div class=" py-9 flex justify-center items-center">
+      <div class=" flex items-center justify-center flex-col w-full">
+        <div class="w-full h-34 flex flex-col items-center">
+          <h1 class="text-3xl md:text-7xl lg:text-7xl font-bold text-red-600 pb-3 md:pb-10 lg:pb-15 xl:">65 % OFF</h1>
+          <div class="w-72 md:w-full text-center">
+            <p class="font-inter text-base md:text-5xl lg:text-7xl p-1 md:pb-7 lg:pb-7 text-red-500 font-extralight">our holiday deals are better</p>
+            <p class=" text-center text-base md:text-5xl lg:text-5xl font-bold text-black textce">All categories</p>
           </div>
         </div>
-        <div>
+        <div class=" md:w-3/5">
           <img src={DealImg} alt="deal" />
         </div>
       </div>
